831d3d592fce7a1cb3c11bcb21da477f91c57831
jiang <zsgwsjj@qq.com>
2018-01-29 17:54:55 +0800

    jiang

---
+++ user/src/main/java/com/zsgwsjj/jiang/user/ctrl/UserAccCtrl.java
@@ -0,0 +1,15 @@
+package com.zsgwsjj.jiang.user.ctrl;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : jiang
+ * @time : 2018/1/12 22:01
+ */
+@RestController
+@RequestMapping("/user")
+public class UserAccCtrl {
+
+
+}
