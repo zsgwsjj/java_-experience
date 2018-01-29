eb266217c077dc22fa319192cae11ea39544aa00
jiang <zsgwsjj@qq.com>
2018-01-29 18:14:21 +0800

    test

--- user/src/main/java/com/zsgwsjj/jiang/user/ctrl/UserAccCtrl.java
+++ user/src/main/java/com/zsgwsjj/jiang/user/ctrl/UserAccCtrl.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserAccCtrl {
 
-    public void first() {
+    public String register() {
+        return null;
     }
 }
